f685a348f039d3c9c903233ec7f20c5f160a135f
karolwasemann <kw.kwaq@gmail.com>
2020-09-30 11:36:05 +0200

    JS with bnt add & remove

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,25 @@ registerSW();
 
 /* place your code below */
 
-console.log("HELLO 🚀");
+const numberGlass = document.querySelector(".glass__number--js");
+const addGlass = document.querySelector(".buttons__add--js");
+const remove = document.querySelector(".buttons__remove--js");
+
+const key = new Date().toISOString().slice(0, 10);
+
+let number = 0;
+
+addGlass.addEventListener("click", () => {
+  console.log("asd");
+  number++;
+  numberGlass.innerHTML = number;
+});
+
+remove.addEventListener("click", () => {
+  if (number > 0) {
+    number--;
+    numberGlass.innerHTML = number;
+  } else {
+    number = 0;
+  }
+});
