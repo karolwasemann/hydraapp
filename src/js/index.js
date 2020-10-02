0f43b9acf8362198a55eed4d10780952515d0ce5
karolwasemann <kw.kwaq@gmail.com>
2020-10-02 12:40:31 +0200

    Add function chcek lvl

--- src/js/index.js
+++ src/js/index.js
@@ -10,32 +10,47 @@ const numberGlass = document.querySelector(".glass__number--js");
 const addGlass = document.querySelector(".buttons__add--js");
 const remove = document.querySelector(".buttons__remove--js");
 const key = new Date().toLocaleString().slice(0, 10);
+const level = document.querySelector(".level__circle--js");
 
-let number = 0;
+let qNumber = 0;
+
+// Poziom nawodnienia
 
 // Sprawdzanie
 
 const localStorageValue = localStorage.getItem(key);
 
 if (localStorageValue) {
-  number = localStorageValue;
+  qNumber = localStorageValue;
 } else {
   localStorage.setItem(key, 0);
 }
-numberGlass.innerHTML = number;
+numberGlass.innerHTML = qNumber;
 
 //Dodawanie i usuwanie szklanek
 
 addGlass.addEventListener("click", () => {
-  number++;
-  numberGlass.innerHTML = number;
-  localStorage.setItem(key, number);
+  qNumber++;
+  numberGlass.innerHTML = qNumber;
+  localStorage.setItem(key, qNumber);
+  checkLevel();
 });
 
 remove.addEventListener("click", () => {
-  if (number > 0) {
-    number--;
+  if (qNumber > 0) {
+    qNumber--;
   }
-  numberGlass.innerHTML = number;
-  localStorage.setItem(key, number);
+  numberGlass.innerHTML = qNumber;
+  localStorage.setItem(key, qNumber);
+  checkLevel();
 });
+
+const checkLevel = () => {
+  if (qNumber <= 3) {
+    level.style.backgroundColor = "red";
+  } else if (qNumber <= 5) {
+    level.style.backgroundColor = "orange";
+  } else {
+    level.style.backgroundColor = "green";
+  }
+};
