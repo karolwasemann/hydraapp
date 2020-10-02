483d19c75496fc9393520173f318c791429248d8
karolwasemann <kw.kwaq@gmail.com>
2020-10-02 10:34:13 +0200

    LocalStorage

--- src/js/index.js
+++ src/js/index.js
@@ -9,22 +9,33 @@ registerSW();
 const numberGlass = document.querySelector(".glass__number--js");
 const addGlass = document.querySelector(".buttons__add--js");
 const remove = document.querySelector(".buttons__remove--js");
-
-const key = new Date().toISOString().slice(0, 10);
+const key = new Date().toLocaleString().slice(0, 10);
 
 let number = 0;
 
+// Sprawdzanie
+
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue) {
+  number = localStorageValue;
+} else {
+  localStorage.setItem(key, 0);
+}
+numberGlass.innerHTML = number;
+
+//Dodawanie i usuwanie szklanek
+
 addGlass.addEventListener("click", () => {
-  console.log("asd");
   number++;
   numberGlass.innerHTML = number;
+  localStorage.setItem(key, number);
 });
 
 remove.addEventListener("click", () => {
   if (number > 0) {
     number--;
-    numberGlass.innerHTML = number;
-  } else {
-    number = 0;
   }
+  numberGlass.innerHTML = number;
+  localStorage.setItem(key, number);
 });
